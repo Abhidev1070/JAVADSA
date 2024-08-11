00413832b3b387fae98aebf9e6bd75996fe19657
Abhidev1070 <abhishekdewda506@gmail.com>
2024-08-11 16:45:18 +0530

    DSA start frist commit

---
+++ DSA.java
@@ -0,0 +1,48 @@
+/**
+ * DSA
+ */
+import java.util.*;
+public class DSA {
+
+  public static void main(String[] args) {
+    // System.out.print(" *\n **\n ***\n ****\n");
+
+    // int a = 45;
+    // int b = 25;
+
+    // int sum = a+b;
+    // System.out.println(sum);
+
+    //  int diff = a-b;
+    // System.out.println(diff);
+
+    // int mlu = a*b;
+    // System.out.println(mlu);
+// int div = a/b;
+// System.out.println(div);
+
+/// Input 
+
+// Scanner sc = new Scanner(System.in);
+// String name = sc.nextLine();
+// System.out.println(name);
+
+Scanner abhi = new Scanner(System.in);
+
+int a = abhi.nextInt();
+int b = abhi.nextInt();
+int sum = a+b;
+System.out.println(sum);
+
+
+
+
+
+
+
+  }
+   
+    
+}
+ 
+
