a19f0d05d7e3d4f5678566821af916487c533ab0
Abhidev1070 <abhishekdewda506@gmail.com>
2024-11-02 07:22:55 +0530

    practice

--- Practice.JS/Functions.js
+++ Practice.JS/Functions.js
@@ -11,21 +11,33 @@
 //   console.log(generator.next().value); 
 //   console.log(generator.next().value);
 
-////questions 2 ////
-function sum(a, b) {
-  return a + b;
-}
+// ////questions 2 ////
+// function sum(a, b) {
+//   return a + b;
+// }
 
-console.log(`1 + 2 = ${sum(1, 2)}.`); 
+// console.log(`1 + 2 = ${sum(1, 2)}.`); 
 
-let guestList = `Guests:
- * John
- * Pete
- * Mary
-`;
-console.log(guestList); 
+// let guestList = `Guests:
+//  * John
+//  * Pete
+//  * Mary
+// `;
+// console.log(guestList); 
 
 
 // let guestList = "Guests: 
 //   * John";
+
+////questions 3 ////
+
+function calculate(a, b) {
+  var c = a + b;
+  return c;
+}
+
+var x = 5;
+var y = 3;
+var result = calculate(x, y);
+console.log(result); 
 
