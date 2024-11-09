282d1c184b55da4b278ac8e098977268e8305c87
Abhidev1070 <abhishekdewda506@gmail.com>
2024-11-09 10:35:49 +0530

    Callback

--- Practice.JS/Callback.js
+++ Practice.JS/Callback.js
@@ -15,3 +15,15 @@
 // }
 // console.log(processData("hello world", toUpperCase)); 
 
+// //function 3// //
+
+function fetchData(callback) {
+    setTimeout(() => {
+      const data = { name: "John Doe", age: 30 };
+      callback(data);
+    }, 2000);
+  }
+  function processData(data) {
+    console.log("Data:", data);
+  }
+  fetchData(processData);
