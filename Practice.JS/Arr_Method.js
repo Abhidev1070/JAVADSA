ca959d881bd982ea0473dfbdb4478db931d67c4c
Abhidev1070 <abhishekdewda506@gmail.com>
2024-11-09 10:40:57 +0530

    Array Method

--- Practice.JS/Arr_Method.js
+++ Practice.JS/Arr_Method.js
@@ -24,3 +24,57 @@
 // console.log(arr.fill("Abhi",2));
 // console.log(arr.fill("Abhi",0,2));
 // console.log(arr.find()); // not andstend 
+
+
+/////////////
+
+class CustomArray {
+    constructor() {
+        this.data = {};     
+        this.length = 0;    
+    }
+
+    // Get an item from our array
+    get(index) {
+        return this.data[index];
+    }
+
+    // Add an item to the end
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    // Remove the last item and return it
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    // Delete item at a specific index
+    deleteAtIndex(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+
+    // Shift items to the left from the specified index
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+}
+
+const newArray = new CustomArray();
+newArray.push(5);
+newArray.push(10);
+newArray.push(15);
+console.log(newArray.get(1)); // Outputs: 10
+newArray.deleteAtIndex(1);
+console.log(newArray.get(1)); // Outputs: 15
