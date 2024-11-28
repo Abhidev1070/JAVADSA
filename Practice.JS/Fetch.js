ab4de2d79cd8b9c1b8f9c5172ae1d215d2e5ba4e
Abhidev1070 <abhishekdewda506@gmail.com>
2024-11-28 13:56:50 +0530

    practice

--- Practice.JS/Fetch.js
+++ Practice.JS/Fetch.js
@@ -35,3 +35,14 @@
   // };
 
   // fetchData();
+
+  //// API 4 ////
+  
+  function fetchData(url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+  
+  fetchData('https://api.example.com/data');
