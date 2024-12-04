5bb52c2cb0e9a4b16972d71764b6ffc1ecfed200
Abhidev1070 <abhishekdewda506@gmail.com>
2024-12-04 20:46:32 +0530

    proctice

--- Practice.JS/Logic.js
+++ Practice.JS/Logic.js
@@ -1065,20 +1065,50 @@
 ////questions 58////
 
 
-function Person(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.getFullName = function () {
-      return `${this.firstName} ${this.lastName}`;
-    };
-  }
+// function Person(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.getFullName = function () {
+//       return `${this.firstName} ${this.lastName}`;
+//     };
+//   }
   
-  function Man(firstName, lastName) {
-    Person.call(this, firstName, lastName); // calling Person firstName, lastName
-    this.gender = 'Man';
-  }
+//   function Man(firstName, lastName) {
+//     Person.call(this, firstName, lastName); // calling Person firstName, lastName
+//     this.gender = 'Man';
+//   }
   
-  const eric = new Man('Eric', 'Wilson');
-  console.log(eric.firstName + ' ' + eric.lastName); // returns Eric + Wilson
+//   const eric = new Man('Eric', 'Wilson');
+//   console.log(eric.firstName + ' ' + eric.lastName); // returns Eric + Wilson
   
-  console.log(eric.getFullName()); // method call returns Full Name: Eric Wilson
+//   console.log(eric.getFullName()); // method call returns Full Name: Eric Wilson
+
+////questions 59////
+
+
+// Array of students with their names and grades
+var students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+var Avgmarks = 0;
+
+// Loop through the students array to calculate the total marks
+for (var i = 0; i < students.length; i++) {
+    Avgmarks += students[i][1];
+    var avg = (Avgmarks / students.length);
+}
+
+// Calculate and log the average grade
+console.log("Average grade: " + (Avgmarks) / students.length);
+
+// Determine the grade based on the average
+if (avg < 60) {
+    console.log("Grade: F");
+} else if (avg < 70) {
+    console.log("Grade: D");
+} else if (avg < 80) {
+    console.log("Grade: C");
+} else if (avg < 90) {
+    console.log("Grade: B");
+} else if (avg <= 100) {
+    console.log("Grade: A");
+} 
