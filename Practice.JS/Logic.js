71da1c3c5f797add280578be0e4951e3c2d13d68
Abhidev1070 <abhishekdewda506@gmail.com>
2024-12-07 12:38:58 +0530

    practice

--- Practice.JS/Logic.js
+++ Practice.JS/Logic.js
@@ -1134,12 +1134,34 @@
 ////questions 61//// 
 
 
-const object1 = {};
+// const object1 = {};
 
-console.log(Object.isExtensible(object1));
-// expected output: true
+// console.log(Object.isExtensible(object1));
+// // expected output: true
 
-Object.preventExtensions(object1);
+// Object.preventExtensions(object1);
 
-console.log(Object.isExtensible(object1));
-// expected output: false
+// console.log(Object.isExtensible(object1));
+// // expected output: false
+
+////questions 62//// 
+
+
+let user = {
+    get name() {
+      return this._name;
+    },
+  
+    set name(value) {
+      if (value.length < 4) {
+        console.log('Name is too short, need at least 4 characters');
+        return;
+      }
+      this._name = value;
+    },
+  };
+  
+  user.name = 'Pete';
+  console.log(user.name); // Pete
+  
+  user.name = ''; // Name is too short...
